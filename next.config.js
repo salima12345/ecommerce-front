d5e2157cc80fc78d2724873513e934ab62e1c89f
your name <you@example.com>
2023-11-15 13:35:17 +0100

    fixed stuff

--- next.config.js
+++ next.config.js
@@ -1,30 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
-    reactStrictMode: true,
-  
-    images:{
-        domains:['res.cloudinary.com']
-    },
-    compiler:{
-        styledComponents:true,
-
-    },
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-          return config;
-        }
-    
-        config.resolve.alias = {
-          ...config.resolve.alias,
-          'react-card-slider-component/dist/Slider.css': false,
-        };
-    
-        return config;
-      },
-    
-   
-}
-
-
-module.exports = nextConfig
+  reactStrictMode: true,
+  images:{
+    domains:['res.cloudinary.com']
+  },
+  compiler:{
+    styledComponents:true,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      return config;
+    }
+ 
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'react-card-slider-component/dist/Slider.css': false,
+    };
+ 
+    return config;
+  },
+  experimental: {
+    largePageDataBytes: 256 * 1024, 
+  },
+ }
+ 
+ module.exports = nextConfig
+ 
