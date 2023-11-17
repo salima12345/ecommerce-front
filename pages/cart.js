3c90c080d503bca091ccdd773db00edfbb1c665e
your name <you@example.com>
2023-11-17 15:34:42 +0100

    fixed stuff

--- pages/cart.js
+++ pages/cart.js
@@ -8,6 +8,7 @@ import { useRouter } from 'next/router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import Image from "next/image";
+import axios from "axios";
 
 
 
