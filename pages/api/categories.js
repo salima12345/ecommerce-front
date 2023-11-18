a07b49a511a3506382d44e226f81c23aa504b9ed
your name <you@example.com>
2023-11-18 15:35:36 +0100

    fixed stuff

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -9,10 +9,8 @@ export default async function handler(req, res) {
 
   try {
     
-    const query = Caterogy.find({ parent: parentId });
-    query.timeout(50000);
-    const childCategories = await query.exec();
-        res.status(200).json(childCategories);
+    const childCategories = await Caterogy.find({ parent: parentId });
+    res.status(200).json(childCategories);
   } catch (error) {
     console.error("Error fetching child categories:", error);
     res.status(500).json({ error: "Internal Server Error" });
