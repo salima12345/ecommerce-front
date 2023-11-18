8c3611b4b26890387724d605d822345a404dbb7e
your name <you@example.com>
2023-11-18 14:17:04 +0100

    fixed stuff

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
 
   try {
     
-    const childCategories = await Caterogy.find({ parent: parentId }).timeout(10000);
+    const childCategories = await Caterogy.find({ parent: parentId }).timeout(50000);
     res.status(200).json(childCategories);
   } catch (error) {
     console.error("Error fetching child categories:", error);
