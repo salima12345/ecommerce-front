95381f70d14d672969887f884112f833562f1ad6
your name <you@example.com>
2023-11-18 15:52:44 +0100

    fixed stuff

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,18 +1,18 @@
-import { Caterogy } from "../../models/Category"; 
+import { Caterogy } from "../../models/Category";
 
 export default async function handler(req, res) {
-  if (req.method !== "GET") {
-    return res.status(405).end(); 
-  }
+ if (req.method !== "GET") {
+   return res.status(405).end(); 
+ }
 
-  const parentId = req.query.parentId;
+ const parentId = req.query.parentId;
 
-  try {
-    
-    const childCategories = await Caterogy.find({ parent: parentId });
-    res.status(200).json(childCategories);
-  } catch (error) {
-    console.error("Error fetching child categories:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+ try {
+   const query = Caterogy.find({ parent: parentId });
+   const childCategories = await query.exec({ maxTimeMS: 50000 });
+   res.status(200).json(childCategories);
+ } catch (error) {
+   console.error("Error fetching child categories:", error);
+   res.status(500).json({ error: "Internal Server Error" });
+ }
 }
