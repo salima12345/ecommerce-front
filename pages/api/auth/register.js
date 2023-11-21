36df0c110dc6275ab9ef37c493b3bf485a52ce43
your name <you@example.com>
2023-11-21 15:34:05 +0100

    fixed stuff

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -7,39 +7,39 @@ export default async function handler(req, res) {
  const { name, email, password } = req.body;
 
  try {
-  // Check if the user with the same email already exists
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    return res.status(400).json({ message: 'Email already in use' });
-  }
+ // Check if the user with the same email already exists
+ const existingUser = await User.findOne({ email });
+ if (existingUser) {
+   return res.status(400).json({ message: 'Email already in use' });
+ }
 
-  // Hash the password with 10 salt rounds
-  const hashedPassword = await bcrypt.hash(password, 10);
+ // Hash the password with 10 salt rounds
+ const hashedPassword = await bcrypt.hash(password, 10);
 
-  // Create a new user
-  const user = new User({ name, email, password: hashedPassword });
+ // Create a new user
+ const user = new User({ name, email, password: hashedPassword });
 
-  // Save the user in the background
-  setTimeout(async () => {
-    try {
-      await user.save();
+ // Save the user in the background
+ setTimeout(async () => {
+   try {
+     await user.save();
 
-      // Issue a JWT token
-      const token = jwt.sign({ userId: user._id }, 'Salima@2001', { expiresIn: '1h' });
+     // Issue a JWT token
+     const token = jwt.sign({ userId: user._id }, 'Salima@2001', { expiresIn: '1h' });
 
-      // Set the token as an HTTP-only cookie
-      res.setHeader('Set-Cookie', `JwtToken=${token}; HttpOnly; Secure; SameSite=Strict; path=/;`);
+     // Set the token as an HTTP-only cookie
+     res.setHeader('Set-Cookie', `JwtToken=${token}; HttpOnly; Secure; SameSite=Strict; path=/;`);
 
-      // Send the response
-      res.status(201).json({ message: 'User registered successfully' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error registering user', error: error.message });
-    }
-  }, 0);
+     // Send the response
+     res.status(201).json({ message: 'User registered successfully' });
+   } catch (error) {
+     console.error(error);
+     res.status(500).json({ message: 'Error registering user', error: error.message });
+   }
+ }, 0);
 
-  // Return a response immediately
-  res.status(202).json({ message: 'User registration in progress' });
+ // Return a response immediately
+ res.status(202).json({ message: 'User registration in progress' });
  } catch (error) {
  console.error(error);
  res.status(500).json({ message: 'Error registering user', error: error.message });
