4ba0cb5274e532a86c114679c90962fac52d0b29
your name <you@example.com>
2023-11-21 15:43:15 +0100

    fixed stuff

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -19,27 +19,14 @@ export default async function handler(req, res) {
  // Create a new user
  const user = new User({ name, email, password: hashedPassword });
 
- // Save the user in the background
- setTimeout(async () => {
-  try {
-    await user.save();
+ // Save the user and issue a JWT token
+ const token = jwt.sign({ userId: user._id }, 'Salima@2001', { expiresIn: '1h' });
 
-    // Issue a JWT token
-    const token = jwt.sign({ userId: user._id }, 'Salima@2001', { expiresIn: '1h' });
+ // Set the token as an HTTP-only cookie
+ res.setHeader('Set-Cookie', `JwtToken=${token}; HttpOnly; Secure; SameSite=Strict; path=/;`);
 
-    // Set the token as an HTTP-only cookie
-    res.setHeader('Set-Cookie', `JwtToken=${token}; HttpOnly; Secure; SameSite=Strict; path=/;`);
-
-    // Send the response
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error registering user', error: error.message });
-  }
- }, 0);
-
- // Return a response immediately
- res.status(202).json({ message: 'User registration in progress' });
+ // Send the response
+ res.status(201).json({ message: 'User registered successfully' });
  } catch (error) {
  console.error(error);
  res.status(500).json({ message: 'Error registering user', error: error.message });
