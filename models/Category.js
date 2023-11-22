66027e35c3820ebade77eaa5c3e76101ab0b2edc
your name <you@example.com>
2023-11-22 12:57:41 +0100

    fixed stuff

--- models/Category.js
+++ models/Category.js
@@ -4,7 +4,6 @@ const CategorySchema=new Schema({
     name:{type:String, required:true},
     parent:{type:mongoose.Types.ObjectId, ref:'Category'},
     image: [{type:String}],
- 
     properties: [{type:Object}]
 
 
