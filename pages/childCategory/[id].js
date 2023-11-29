f8d9caf116abb07642feac29652a676a6c4fc5eb
your name <you@example.com>
2023-11-29 17:46:42 +0100

    fixed stuff

--- pages/childCategory/[id].js
+++ pages/childCategory/[id].js
@@ -7,7 +7,6 @@ import { primary } from "../../lib/colors";
 import { AiOutlineCaretUp, AiOutlineCaretDown } from 'react-icons/ai';
 import 'react-icons/ai';
 import { useRef, useEffect } from 'react';
-import Image from 'next/image';
 
 import Slider from 'rc-slider';
 
